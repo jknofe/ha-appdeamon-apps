a15ad7043dd5eed2e98c9edd07ab402f95313865
Jojo <Johannes.knofe@gmail.com>
2025-03-14 19:31:09 +0100

    Refactor PowerMeter app to use float for power variables and add logging for garage power consumption

--- PowerMeter.py
+++ PowerMeter.py
@@ -7,12 +7,12 @@ class PowerMeter(hass.Hass):
         self.log("PowerMeter App Started!")
         self.run_every(self.query_power_meters, "now", 3)  # Runs every 3 second
 
-        self.power_ph_a = 0
-        self.power_ph_b = 0
-        self.power_ph_c = 0
-        self.power_ph_sum = 0
-        self.power_garage = 0
-        self.power_solar = 0
+        self.power_ph_a = 0.0
+        self.power_ph_b = 0.0
+        self.power_ph_c = 0.0
+        self.power_ph_sum = 0.0
+        self.power_garage = 0.0
+        self.power_solar = 0.0
     
     def _simple_ema_filter(value, ema, alpha):
         """Simple Exponential Moving Average filter."""
@@ -27,6 +27,7 @@ class PowerMeter(hass.Hass):
         try:
             # Read data from Home Assistant sensor
             self.power_garage = self.get_state("sensor.fritz_dect_200_1_power_consumption")
+            self.log(f"Garage: G={self.power_garage}W")
         except Exception as e:
             self.log(f"Error fetching sensor.fritz_dect_200_1_power_consumption: {e}")
             self.power_garage = 0
@@ -35,9 +36,9 @@ class PowerMeter(hass.Hass):
         try:
             response1 = requests.get(url_3em, timeout=2)
             data1 = response1.json()
-            power_ph_a = data1.get("a_act_power", 0)
-            power_ph_b = data1.get("b_act_power", 0)
-            power_ph_c = data1.get("c_act_power", 0)
+            power_ph_a = float(data1.get("a_act_power", 0))
+            power_ph_b = float(data1.get("b_act_power", 0))
+            power_ph_c = float(data1.get("c_act_power", 0))
 
             # Update Home Assistant sensors
             #self.set_state("sensor.a_act_power", state=a_act_power, unit_of_measurement="W")
